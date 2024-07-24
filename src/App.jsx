0db835008a1153bb0973a9734d5588984ce5d559
Lorenzo-Foschetti <lorenzo.foschetti00@gmail.com>
2024-07-24 16:13:01 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MyNavbar from "./components/MyNavbar";
+
+function App() {
+  return (
+    <div className="App">
+      <MyNavbar />
+    </div>
+  );
+}
+
+export default App;
